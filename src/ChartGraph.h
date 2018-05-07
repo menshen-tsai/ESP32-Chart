d0f6f29952be8402a51516ef8df73a26b57d397a
user <user@mail>
2018-05-07 09:03:22 +0800

    Undo removing UNIT_LEN since it is still needed in the ChartGraph class.  Change the variable names to be more consistant.

--- src/ChartGraph.h
+++ src/ChartGraph.h
@@ -5,15 +5,15 @@
 #ifndef _CHARTGRAPH_
 #define _CHARTGRAPH_
 
-#define max_readings 200
-#define xticks      5                // 5 x-axis division markers
-#define yticks      5                // 5 y-axis division markers
+#define max_readings   200
+#define xticks        5                // 5 x-axis division markers
+#define yticks        5                // 5 y-axis division markers
 #define xGraphOffset  100
-
+#define UNIT_LEN	  10
 
 struct DataPoint {
   float    data;
-  time_t    timeStamp;
+  time_t   timeStamp;
 };
 class ChartGraph {
   public:
@@ -32,19 +32,19 @@ class ChartGraph {
   private:
     uint16_t _index;
     uint16_t _max_readings;
-    boolean _autoScale;
+    boolean  _autoScale;
     uint16_t _graphColor;
     uint16_t _backgroundColor;
     uint16_t _xPos, _yPos, _width, _height;
     uint16_t _y1Max;
     uint16_t _y1Min;    
-    String _Title;
-    boolean _barchartMode;
-    float _DataArray[max_readings];
+    String   _Title;
+    boolean  _barchartMode;
+    float    _DataArray[max_readings];
     //float _Data[max_readings];
     struct DataPoint _Data[max_readings];
-    int Old_x2[max_readings+1];
-    int Old_y2[max_readings+1];
+    int _Old_x2[max_readings+1];
+    int _Old_y2[max_readings+1];
     long _xScale[6];
     uint32_t _timeTag;
     uint8_t _xScaleIndex;
